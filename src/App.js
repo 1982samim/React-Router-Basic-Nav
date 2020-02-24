3b3a48b19d9dda2e83a938f376cca8845cba5a1f
Sami Web28 <58615068+1982samim@users.noreply.github.com>
2020-02-24 13:06:52 -0800

    React basic Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Router} from 'react-router-dom'
 
 const App = () => (
   <div>
     <Navigation />
+    {/* Home Router */}
+    <Router exact path='/'>
+      <Home />
+    </Router>
+
+    {/* About Router */}
+    <Router path='/about'>
+      <About />
+    </Router>
+
+    {/* Contact Router */}
+    <Router path='/contact'>
+      <Contact />
+    </Router>
   </div>
 );
 
